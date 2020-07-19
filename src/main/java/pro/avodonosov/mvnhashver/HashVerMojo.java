b2a71ce52589c2ac034ca495231dc19d64a01b1c
Anton Vodonosov <avodonosov@yandex.ru>
2020-07-19 04:31:34 +0300

    An "MVP" version. 3 artifact existence check methods implemented, "resolve" by default. System property values for the version property expressions are populated during MavenLifecycleParticipant.afterSessionStart from two property files: <prjRoot>/versions.properties and <prjRoot>/target/hashversions.properties. The first file is expected to be committed to the source control and it contains versions for project members who don't use hashversions. The second one is produces by the "hashver" mojo.

--- src/main/java/pro/avodonosov/mvnhashver/HashVerMojo.java
+++ src/main/java/pro/avodonosov/mvnhashver/HashVerMojo.java
@@ -8,6 +8,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.DefaultProjectBuildingRequest;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuildingRequest;
@@ -18,6 +19,7 @@ import org.apache.maven.shared.dependency.graph.traversal.DependencyNodeVisitor;
 import org.apache.maven.shared.dependency.graph.traversal.SerializingDependencyNodeVisitor;
 
 import javax.inject.Inject;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -31,6 +33,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Collections;
 import java.util.Comparator;
@@ -40,11 +43,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+// TODO: Investigate the "Downloading " message for reactor modules
+//       of maven-wagon project during the "hashver" mojo execution
+//       (seems happening only for the dependency-managed modules):
+//       >
+//       >    Downloading: https://repo.maven.apache.org/maven2/org/apache/maven/wagon/wagon-http-shared/4y2Na53WvxoBMoDdnku6ZIN61UM.P87Jh9SiFF__5rM-TAGdYaFzv6U/wagon-http-shared-4y2Na53WvxoBMoDdnku6ZIN61UM.P87Jh9SiFF__5rM-TAGdYaFzv6U.jar
+//       >
 @Mojo(name = "hashver", aggregator = true)
 public class HashVerMojo extends AbstractMojo {
 
     public static final String DIGEST_ALGO = "SHA-1";
 
+    @Parameter(defaultValue = "false", property = "includeGroupId")
+    boolean includeGroupId;
+    
     /**
      * The dependency tree builder to use.
      */
@@ -56,6 +68,10 @@ public class HashVerMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        // TODO: Include build options into the hash,
+        //       explicitly specified and maybe some automatically.
+        //       Explicit specification can probably include not only
+        //       values, but also property references.
         
         Map<String, String> ownHashByArtifact = new HashMap<>();
         for (MavenProject prj : mavenSession.getProjects()) {
@@ -74,8 +90,8 @@ public class HashVerMojo extends AbstractMojo {
         for (MavenProject prj : mavenSession.getProjects()) {
             try {
                 hashVers.put(
-                        prj.getGroupId() + "." + prj.getArtifactId() + ".hashver",
-                        prjVersion(mavenSession, prj,
+                        hashVerKey(prj),
+                        prjHash(mavenSession, prj,
                                 dependencyGraphBuilder, ownHashByArtifact));
             } catch (DependencyGraphBuilderException e) {
                 throw new MojoExecutionException(
@@ -84,11 +100,11 @@ public class HashVerMojo extends AbstractMojo {
             }
         }
 
-        getLog().info("HashVers computed: " + hashVers.size());
+        logInfo("HashVers computed: " + hashVers.size());
         ArrayList<String> keys = new ArrayList<>(hashVers.keySet());
         Collections.sort(keys);
         for (String prjKey : keys) {
-            getLog().info(prjKey + "=" + hashVers.get(prjKey));
+            logInfo(prjKey + "=" + hashVers.get(prjKey));
         }
 
         try {
@@ -98,24 +114,72 @@ public class HashVerMojo extends AbstractMojo {
         }
     }
 
+    private void logInfo(String msg) {
+        getLog().info(msg);
+    }
+
+    private String hashVerKey(MavenProject prj) {
+        return includeGroupId
+                ? prj.getGroupId() + "." + prj.getArtifactId() + ".version"
+                : prj.getArtifactId() + ".version";
+    }
+
     private void storeHashVers(Map<String, String> hashVers)
             throws IOException
     {
-        storeHashVerProps(hashVers, "hashversions.properties");
-        storeMvnEx(hashVers, "mvnex.sh");
+        storeHashVerProps(hashVers, "target/hashversions.properties");
+        //storeMavenConfig(hashVers, ".mvn/maven.config");
+        //storeMvnEx(hashVers, "mvnex.sh");
+    }
+    
+    private static List<String> sortedKeys(Map<String, ?> hashVers) {
+        ArrayList<String> keys = new ArrayList<>(hashVers.keySet());
+        keys.sort(String::compareTo);
+        return keys;
+    }
+    
+    private static void ensureParentDirExists(String file) {
+        File parentDir = new File(file).getParentFile();
+        if (parentDir != null) {
+            // parentDir can be null when the file
+            // doesn't specify parent (meaning a file in the current directory)
+
+            parentDir.mkdirs();
+        }
+    }
+
+    private void storeMavenConfig(Map<String, String> hashVers,
+                                  String file)
+            throws IOException
+    {
+        ArrayList<String> keys = new ArrayList<>(hashVers.keySet());
+        keys.sort(String::compareTo);
+        try (BufferedWriter out = openWriter(file)) {
+            for (String key : sortedKeys(hashVers)) {
+                out.write("-D");
+                out.write(key);
+                out.write("=");
+                out.write(hashVers.get(key));
+                out.newLine();
+            }
+        }
+        logInfo("Saved hasVers to " + file);
+    }
+    
+    private static BufferedWriter openWriter(String file) throws IOException {
+        ensureParentDirExists(file);
+        return new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(file),
+                                        StandardCharsets.UTF_8));
     }
 
     private void storeMvnEx(Map<String, String> hashVers, String file)
             throws IOException
     {
-        ArrayList<String> keys = new ArrayList<>(hashVers.keySet());
-        keys.sort(String::compareTo);
-        try (Writer out = new OutputStreamWriter(
-                new FileOutputStream(file), StandardCharsets.UTF_8))
-        {
+        try (Writer out = openWriter(file)) {
             out.write("#!/bin/sh\n");
             out.write("mvn");
-            for (String key : keys) {
+            for (String key : sortedKeys(hashVers)) {
                 out.write(" ");
                 out.write("-D");
                 out.write(key);
@@ -124,9 +188,11 @@ public class HashVerMojo extends AbstractMojo {
             }
             out.write(" \"$@\"");
         }
+        logInfo("Saved hasVers to " + file);
     }
 
-    private void storeHashVerProps(Map<String, String> hashVers, String file)
+    private void storeHashVerProps(Map<String, String> hashVers,
+                                   String file)
             throws IOException
     {
         Properties props = new Properties() {
@@ -141,11 +207,14 @@ public class HashVerMojo extends AbstractMojo {
                 return Collections.enumeration(keys);
             }
         };
-
         props.putAll(hashVers);
+
+        ensureParentDirExists(file);
         try (OutputStream out = new FileOutputStream(file)) {
             props.store(out, null);
         }
+
+        logInfo("Saved hasVers to " + file);
     }
 
     private static String ownHash(MavenProject module)
@@ -168,6 +237,10 @@ public class HashVerMojo extends AbstractMojo {
             throws IOException
     {
         System.out.println("directoryHash: " + dir.getPath());
+        // TODO: only digest names relative to module root,
+        //       to make the hash independent on work directory / machine
+        // TODO: make the hash independent of file.separator
+        // TODO: make the directory children sort order independent of the OS
         digest.update(dir.getName().getBytes(StandardCharsets.UTF_8));
 
         File[] children = dir.listFiles();
@@ -175,6 +248,7 @@ public class HashVerMojo extends AbstractMojo {
             throw new IOException(dir.getPath() + " is not a directory");
         }
 
+        Arrays.sort(children);
         for (File child : children) {
             if (child.isDirectory()) {
                 directoryHash(child, digest);
@@ -194,29 +268,32 @@ public class HashVerMojo extends AbstractMojo {
             byte[] buf = new byte[10240];
             int len;
             while ((len = in.read(buf)) != -1) {
-                digest.update(buf, 0, len);
+                if (System.getProperty("hashverDigestSkip") == null) {
+                    digest.update(buf, 0, len);
+                }
             }
         }
     }
 
-    static String prjVersion(MavenSession session,
-                             MavenProject prj,
-                             DependencyGraphBuilder dependencyGraphBuilder,
-                             Map<String, String> ownHashByArtifact)
+    static String prjHash(MavenSession session,
+                          MavenProject prj,
+                          DependencyGraphBuilder dependencyGraphBuilder,
+                          Map<String, String> ownHashByArtifact)
             throws DependencyGraphBuilderException
     {
         ProjectBuildingRequest buildingRequest =
-                new DefaultProjectBuildingRequest(session.getProjectBuildingRequest());
+                new DefaultProjectBuildingRequest(
+                        session.getProjectBuildingRequest());
 
         buildingRequest.setProject(prj);
-
 
         DependencyNode rootNode = dependencyGraphBuilder.buildDependencyGraph(
                 buildingRequest,
                 null,
                 session.getProjects());
 
-        String ownHash = ownHashByArtifact.get(ArtifactUtils.key(prj.getArtifact()));
+        String ownHash = ownHashByArtifact.get(
+                ArtifactUtils.key(prj.getArtifact()));
         if (ownHash == null) {
             throw new RuntimeException(
                     "Can find own hash for module " + prj.getName());
@@ -287,7 +364,7 @@ public class HashVerMojo extends AbstractMojo {
                             ArtifactUtils.key(node.getArtifact()));
 
                     if (ownHash != null) {
-                        return ownHash + " " + node.toNodeString();
+                        return hashVerNodeString(node, ownHash);
                     } else {
                         return node.toNodeString();
                     }
@@ -299,6 +376,54 @@ public class HashVerMojo extends AbstractMojo {
                 }
             });
         }
+    }
+
+    /**
+     * Similar DependencyNode.toNodeString(), only use hash version
+     * instead of the artifact version. (This function is only for
+     * artifacts in the reactor).
+     * 
+     * The goal is to prevent  hashversion to depend on itself - 
+     * prevent *.version property expression
+     * value to sneak into the  dependency tree we hash - otherwise new
+     * hashversion will affect the dependency tree and thus lead
+     * to different hashversion of the artifact.
+     * 
+     * Unlike DependencyNode.toNodeString() we don't include
+     * various "premanaged" properties - they are not important to
+     * the hashversion goal (only the final versions are important), 
+     * and they can also include the *.version property expression.
+     */
+    private static String hashVerNodeString(DependencyNode node, String ownHash) {
+        // assert ownHash != null;
+        
+        StringBuilder result = new StringBuilder();
+
+        Artifact artifact = node.getArtifact();
+
+        if (artifact.getGroupId() != null) {
+            result.append(artifact.getGroupId());
+            result.append(":");
+        }
+        result.append(artifact.getArtifactId());
+        result.append(":");
+        result.append(artifact.getType());
+        if (artifact.hasClassifier()) {
+            result.append(":");
+            result.append(artifact.getClassifier());
+        }
+        result.append(":");
+        result.append(ownHash);
+        if (artifact.getScope() != null ) {
+            result.append( ":" );
+            result.append(artifact.getScope());
+        }
+        
+        if (node.getOptional() != null && node.getOptional()) {
+            result.append(" (optional) ");
+        }
+
+        return result.toString();
     }
 
     private static String dependencyTreeHash(DependencyNode theRootNode,
---
+++ src/main/java/pro/avodonosov/mvnhashver/MavenLifecycleParticipant.java
@@ -0,0 +1,307 @@
+package pro.avodonosov.mvnhashver;
+
+import org.apache.maven.AbstractMavenLifecycleParticipant;
+import org.apache.maven.MavenExecutionException;
+import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.logging.Logger;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.impl.ArtifactResolver;
+import org.eclipse.aether.repository.LocalArtifactRequest;
+import org.eclipse.aether.repository.LocalArtifactResult;
+import org.eclipse.aether.repository.LocalRepositoryManager;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+// TODO:
+//   - help
+//   - by default don't skip modules if the user has specified an explicit
+//     list of projects (-pl)? (gitflow-incremental-builder adopted
+//     this mode).
+@Singleton
+@Named
+@Component(
+        role = AbstractMavenLifecycleParticipant.class,
+        hint = "hashver-extension")
+public class MavenLifecycleParticipant
+        extends AbstractMavenLifecycleParticipant
+{
+
+    @Requirement
+    @Inject
+    private Logger logger;
+
+    @Inject
+    ArtifactResolver artifactResolver;
+
+    @Override
+    public void afterSessionStart(MavenSession session) throws MavenExecutionException {
+        super.afterSessionStart(session);
+        String baseDir = session.getExecutionRootDirectory();
+        propFileToSysIfExists(baseDir + "/versions.properties");
+        propFileToSysIfExists(baseDir + "/target/hashversions.properties");
+    }
+
+    private void propFileToSysIfExists(String file)
+            throws MavenExecutionException
+    {
+        File f = new File(file);
+        if (!f.exists()) {
+            logger.info("File is absent - loading nothing: " + file);
+        } else {
+            Properties props = new Properties();
+            try (InputStream in = new FileInputStream(f)) {
+                props.load(in);
+            } catch (IOException e) {
+                throw new MavenExecutionException(
+                        "Error loading " + file, e);
+            }
+            logInfo("Setting system properties from " + file);
+            for (Object propName : props.keySet()) {
+                logInfo(propName.toString() + "=" + props.get(propName));
+                System.setProperty(
+                        propName.toString(),
+                        props.get(propName).toString());
+            }
+        }
+    }
+
+    @Override
+    public void afterProjectsRead(MavenSession session) {
+         if (!ConfigProps.skipExistingArtifacts.isTrue(session)) {
+            return;
+        }
+
+        logInfo("Preparing to remove from the maven session the modules"
+                + " whose artifacts exist.");
+
+        ExistenceCheckMethod[] existenceChecks =
+                ExistenceCheckMethod.parse(
+                        ConfigProps.existenceCheckMethod.get(session));
+
+//        logInfo("Project Repositories:");
+//        for (MavenProject prj : session.getProjects()) {
+//            logInfo("     " + prj.getArtifactId()
+//                    + ",\n          getDistributionManagementArtifactRepository(): "
+//                    + prj.getDistributionManagementArtifactRepository()
+//                    + "/n          getRemoteArtifactRepositories(): "
+//                    + prj.getRemoteArtifactRepositories()
+//                    + "/n          getRemoteProjectRepositories(): "
+//                    + prj.getRemoteProjectRepositories());
+//        }
+
+        // TODO: detailed logging about artifact search and skipping
+        session.setProjects(
+            session.getProjects().stream()
+                    .filter(prj -> 
+                            "pom".equals(prj.getPackaging()) 
+                                || ! artifactExists(existenceChecks,
+                                                    session,
+                                                    prj))
+                    .collect(Collectors.toList()));
+    }
+
+    private void logInfo(String s) {
+        logger.info("[HASHVER] " + s);
+    }
+
+    private void logInfo(String s, Throwable t) {
+        logger.info("[HASHVER] " + s, t);
+    }
+
+    private static String getProp(MavenSession session,
+                                  String name,
+                                  String defaultValue)
+    {
+        String val = System.getProperty(name);
+        if (val != null) {
+            return val;
+        }
+        Properties projectProps = session.getTopLevelProject().getProperties();
+        return projectProps.getProperty(name, defaultValue);
+    }
+
+    interface ExistenceCheck {
+        boolean artifactExists(MavenSession session, MavenProject project);
+    }
+    
+    ExistenceCheck implementation(ExistenceCheckMethod method) {
+        switch (method) {
+            case resolve:
+                return this::canResolveArtifact;
+            case local:
+                return this::localArtifactExists;
+            case httpHead:
+                return this::canHttpHeadArtifact;
+            default:
+                throw new RuntimeException(
+                        "Unexpected artifact existence check method: " + method);
+        }
+    }
+
+    boolean artifactExists(ExistenceCheckMethod[] methods,
+                           MavenSession session,
+                           MavenProject prj)
+    {
+        for (ExistenceCheckMethod method : methods) {
+            if (implementation(method).artifactExists(session, prj)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static DefaultArtifact aetherArtifact
+            (org.apache.maven.artifact.Artifact  a)
+    {
+        return new DefaultArtifact(
+                a.getGroupId(),
+                a.getArtifactId(),
+                a.getClassifier(),
+                // Is mavenArtifact.getType() == aetherArtifact.getExtension?
+                // (It works, but is that correct or just a coincidence?)
+                a.getType(), 
+                a.getVersion());
+    }
+    
+    boolean canResolveArtifact(MavenSession session, MavenProject prj) {
+        logInfo("resolve-checking existence of " + prj.getArtifact());
+        ArtifactRequest req = new ArtifactRequest(
+                aetherArtifact(prj.getArtifact()),
+                prj.getRemoteProjectRepositories(),
+                null);
+        
+        try {
+            ArtifactResult result = artifactResolver.resolveArtifact(
+                    session.getRepositorySession(),
+                    req);
+            logInfo(
+                    "Resolution result '" + result.isResolved()
+                    + "' for " + prj.getArtifact());
+            return result.isResolved();
+        } catch (ArtifactResolutionException e) {
+            logInfo("Failed to resolve artifact " + req, e);
+            return false;
+        }
+    }
+
+    boolean localArtifactExists(MavenSession session, MavenProject prj) {
+        RepositorySystemSession repoSession = session.getRepositorySession();
+        LocalRepositoryManager lrm = repoSession.getLocalRepositoryManager();
+        LocalArtifactResult result = lrm.find(
+                repoSession,
+                new LocalArtifactRequest(
+                        aetherArtifact(prj.getArtifact()),
+                        null,
+                        null));
+        logInfo("localArtifactExists: " + result.isAvailable()
+                + " for " + prj.getArtifact());
+        return result.isAvailable();
+    }
+
+    /** Joins baseUrl and subPath, making sure exactly one '/' separates them.
+     */
+    static String subUrl(String baseUrl, String subPath) {
+        if (baseUrl.endsWith("/")) {
+            if (subPath.startsWith("/")) {
+                return baseUrl + subPath.substring(1);
+            } else {
+                return baseUrl + subPath;    
+            }
+        } else {
+            if (subPath.startsWith("/")) {
+                return baseUrl + subPath;
+            } else {
+                return baseUrl + "/" + subPath;
+            }
+        }
+    }
+    
+    boolean canHttpHeadArtifact(MavenSession session, MavenProject prj) {
+        logInfo("http-head-checking existence of " + prj.getArtifact());
+        for (RemoteRepository repo : prj.getRemoteProjectRepositories()) {
+            //repo.
+
+            DefaultRepositoryLayout layout = new DefaultRepositoryLayout();
+            URI artifactUri = URI.create(subUrl(
+                    repo.getUrl(), 
+                    layout.pathOf(prj.getArtifact())));
+            try {
+                HttpURLConnection connection =
+                        (HttpURLConnection)artifactUri.toURL().openConnection();
+                connection.setRequestMethod("HEAD");
+                connection.connect();
+                boolean exists = connection.getResponseCode() == 200;
+                logInfo("HTTP HEAD " + (exists ? "successful" : "failed")
+                            + " (" + connection.getResponseCode()
+                            + ") for " + artifactUri);
+                if (exists) {
+                    return true;
+                }
+            } catch (IOException e) {
+                logInfo("Failed to perform HTTP HEAD for "
+                    + artifactUri + " : " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    enum ExistenceCheckMethod {
+        resolve,
+        local,
+        httpHead;
+        
+        public static ExistenceCheckMethod[] parse(String methods) {
+            String[] names = methods.split(",");
+            return Arrays.stream(names)
+                    .map(ExistenceCheckMethod::valueOf)
+                    .toArray(ExistenceCheckMethod[]::new);
+        }
+    }
+
+    enum ConfigProps {
+        skipExistingArtifacts("false"),
+        existenceCheckMethod("resolve");
+
+        public final String defaultValue;
+
+        ConfigProps(String defaultValue) {
+            this.defaultValue = defaultValue;
+        }
+
+        public boolean isTrue(MavenSession session) {
+            String val = get(session);
+            if (val == null) {
+                return false;
+            }
+            if (val.isEmpty()) {
+                return true;
+            }
+            return Boolean.parseBoolean(val);
+        }
+        
+        public String get(MavenSession session) {
+            return getProp(session, name(), defaultValue);
+        }
+
+    }
+}
