33623a37140d1a74ff8a82213ac0c87848291cd5
Anton Vodonosov <avodonosov@yandex.ru>
2020-07-24 04:07:02 +0300

    License header for the MavenLifecycleParticipantTest.java

--- src/test/java/pro/avodonosov/mvnhashver/MavenLifecycleParticipantTest.java
+++ src/test/java/pro/avodonosov/mvnhashver/MavenLifecycleParticipantTest.java
@@ -1,3 +1,22 @@
+/*
+    Copyright 2020 Anton Vodonosov (avodonosov@yandex.ru).
+
+    This file is part of hashver-maven-plugin.
+
+    hashver-maven-plugin is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    hashver-maven-plugin is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with hashver-maven-plugin.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package pro.avodonosov.mvnhashver;
 
 import org.junit.jupiter.api.Test;
